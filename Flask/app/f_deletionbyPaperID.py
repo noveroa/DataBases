39479bcdea1a27869e9f79eab06222cd7837767b
Aileen Novero <aileennovero1@Aileens-Macintosh.local>
2016-08-17 14:06:59 -0400

    adding deletion from keys and affiliation tables

--- Flask/app/f_deletionbyPaperID.py
+++ Flask/app/f_deletionbyPaperID.py
@@ -35,6 +35,113 @@ def deleteRowTABLE(db, paperID, table = 'ABSTRACTSTOTAL'):
         df = r.sqlCMDToPD('ABSTRACTSTOTAL', db)
     
         return df
+def mergeDelKEYS(db, paperID, table1, pk1, table2, pk2, table3, pk3, evaluate):
+    sqlcmd1 = '''
+    SELECT {tn1}.%s, {tn2}.%s
+    FROM {tn1}
+    JOIN {tn2}
+    ON {tn1}.%s = {tn2}.%s
+    '''.format(tn1 = table1, tn2= table2)%(pk1, pk2, pk1, pk1)
+
+    #sqlcmd2a =  ''' SELECT * FROM {tn3} WHERE %s IN %s '''.format(tn3 = table3)%(pk2, values)
+    
+    sqlcmd2 = '''
+    SELECT {tn2}.%s
+    FROM {tn1}
+    JOIN {tn2}
+    ON {tn1}.%s = {tn2}.%s
+    WHERE {tn1}.%s = {myid}
+    '''.format(tn1 = table1, tn2= table2, myid = paperID)%(pk2, pk1, pk1, pk1)
+
+    
+    
+    with sql.connect(db) as con:
+        #get all papers and their keywords
+        df= pd.read_sql(sqlcmd1, con)
+        #get all keywords in my paper
+        df2 = pd.read_sql(sqlcmd2, con)
+        vals = tuple(eval(d).encode('utf-8') for d in df2[pk2])
+        #get the keyword IDs given the keywords
+        sqlcmd3 =  ''' SELECT * FROM {tn3} WHERE %s IN %s '''.format(tn3 = table3)%(pk2, vals)
+        
+        df3 = pd.read_sql(sqlcmd3, con)
+        
+        if evaluate:
+            df[pk2] = df[pk2].apply(lambda k : eval(k))
+        
+        #merge the all papers and keywords with only keywords in paper. If count < = 1, remove from KEYS table
+        c = pd.merge(df, df3)
+        c = pd.DataFrame(c.groupby([pk2,
+                                    pk3]).size()
+                        ).reset_index().rename(columns = {0:'Count'}).query('Count <= 1')
+        
+        return c
+
+
+def deletefromKEYS(paperID, pkcol, db, mpkcol = 'keyID'):
+    keys = mergeDelKEYS(db, paperID, 'PAPER','paperID', 'PAPERKEY', 'keyword', 'KEYS', 'keyID', evaluate = True)
+    #need to delete keys from PAPERKEY based on PaperID now
+    r.deleteRowPK(db, 'PAPERKEY', pkcol, paperID)
+    
+    deleted = [x for x in keys[mpkcol].apply(lambda key: r.deleteRowPK(db = db, 
+                                                                    table = 'KEYS', 
+                                                                    pkcol = mpkcol, 
+                                                                    entryID =key))]
+    return deleted
+
+def mergeDelAffil(db, paperID, table1, pk1, table2, pk2, table3, pk3):
+    sqlcmd1 = '''
+    SELECT {tn1}.%s, {tn2}.%s
+    FROM {tn1}
+    JOIN {tn2}
+    ON {tn1}.%s = {tn2}.%s
+    '''.format(tn1 = table1, tn2= table2)%(pk1, pk2, pk1, pk1)
+
+    #sqlcmd2a =  ''' SELECT * FROM {tn3} WHERE %s IN %s '''.format(tn3 = table3)%(pk2, values)
+    
+    sqlcmd2 = '''
+    SELECT {tn2}.%s
+    FROM {tn1}
+    JOIN {tn2}
+    ON {tn1}.%s = {tn2}.%s
+    WHERE {tn1}.%s = {myid}
+    '''.format(tn1 = table1, tn2= table2, myid = paperID)%(pk2, pk1, pk1, pk1)
+
+    
+    
+    with sql.connect(db) as con:
+        #get all papers and their keywords
+        df= pd.read_sql(sqlcmd1, con)
+        #get all keywords in my paper
+        df2 = pd.read_sql(sqlcmd2, con)
+        vals = df2[pk2].values[0]
+            
+        #get the keyword IDs given the keywords
+        sqlcmd3 =  ''' SELECT * FROM {tn3} WHERE {pkcol} = {my_id} '''.format(tn3 = table3, pkcol = pk2, my_id = vals)
+        
+        df3 = pd.read_sql(sqlcmd3, con)
+        
+        #merge the all papers and keywords with only keywords in paper. If count < = 1, remove from KEYS table
+        c = pd.merge(df, df3)
+        c = pd.DataFrame(c.groupby([pk2,
+                                   pk3]).size()
+                        ).reset_index().rename(columns = {0:'Count'}).query('Count <= 1')
+        
+        return c
+
+
+def deletefromAFFILIATIONS(paperID, pkcol, db, mpkcol = 'affilID'):
+    keys = mergeDelAffil(db, paperID, 'PAPER', 'paperID', 'AFFILIATIONPAPER', 'affilID', 'AFFILIATIONS', 'affiliation')
+    #need to delete keys from PAPERKEY based on PaperID now
+    r.deleteRowPK(db, 'AFFILIATIONPAPER', pkcol, paperID)
+    
+    deleted = [x for x in keys[mpkcol].apply(lambda key: r.deleteRowPK(db = db, 
+                                                                    table = 'AFFILIATIONS', 
+                                                                    pkcol = mpkcol, 
+                                                                    entryID = key
+                                                                     )
+                                             )]
+    return deleted
 
 def deletebyPaper(pk, pkcol = 'paperID', db = DEFAULTDB):
     '''Deleting a Record from datbase by paperID
@@ -44,19 +151,25 @@ def deletebyPaper(pk, pkcol = 'paperID', db = DEFAULTDB):
     '''
 
     print (pk)
-    #delete from composites
-    r.deleteRowPK(db, 'PAPERKEY', pkcol, pk)
-
+    #delete from KEYS
+    deletefromKEYS(pk, pkcol, db)
+    
+    #delete from Affiliations
+    deletefromAFFILIATIONS(pk, pkcol, db)
+    
+    
+    #delete from composites (within deleting from Keys)
+    #r.deleteRowPK(db, 'PAPERKEY', pkcol, pk)
+    #r.deleteRowPK(db, 'AFFILIATIONPAPER', pkcol, pk)
     r.deleteRowPK(db, 'PAPERAUTHOR', pkcol, pk)
 
-    r.deleteRowPK(db, 'AFFILIATIONPAPER', pkcol, pk)
+    
 
     #delete from TOTAL ABSTRACTS
-    
     deleteRowTABLE(db, pk)
 
     #delete the paper from paperTable LAST!
     r.deleteRowPK(db, 'PAPER', pkcol, pk)
     
     print 'Deletion Complete'
-    
+   
